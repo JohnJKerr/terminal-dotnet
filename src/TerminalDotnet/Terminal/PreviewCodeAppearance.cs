6fa51a77a3bbf3fd039c2695152f6b68f8c27ee3
John Kerr <kerr.john@gmail.com>
2026-09-03 20:51:33 +0100

    Preview: Correct syntax highlighting

--- src/TerminalDotnet/Terminal/PreviewCodeAppearance.cs
+++ src/TerminalDotnet/Terminal/PreviewCodeAppearance.cs
@@ -6,18 +6,18 @@ public static class PreviewCodeAppearance
 {
     public static Color ForegroundFor(VisualRole role) => role switch
     {
-        VisualRole.CodeComment => Color.Green,
-        VisualRole.CodeKeyword => Color.BrightMagenta,
-        VisualRole.CodeString => Color.BrightYellow,
-        VisualRole.CodeNumber => Color.BrightCyan,
-        VisualRole.CodeOperator => Color.White,
-        VisualRole.CodeType => Color.BrightBlue,
-        VisualRole.CodePreprocessor => Color.BrightMagenta,
-        VisualRole.CodeIdentifier => Color.BrightCyan,
-        VisualRole.CodeConstant => Color.BrightCyan,
-        VisualRole.CodePunctuation => Color.Gray,
-        VisualRole.CodeFunctionName => Color.BrightBlue,
-        VisualRole.CodeAttribute => Color.BrightYellow,
-        _ => Color.White
+        VisualRole.CodeComment => new(0x56, 0x5f, 0x89),
+        VisualRole.CodeKeyword => new(0x7d, 0xcf, 0xff),
+        VisualRole.CodeString => new(0x9e, 0xce, 0x6a),
+        VisualRole.CodeNumber => new(0xff, 0x9e, 0x64),
+        VisualRole.CodeOperator => new(0x89, 0xdd, 0xff),
+        VisualRole.CodeType => new(0x2a, 0xc3, 0xde),
+        VisualRole.CodePreprocessor => new(0x7d, 0xcf, 0xff),
+        VisualRole.CodeIdentifier => new(0xbb, 0x9a, 0xf7),
+        VisualRole.CodeConstant => new(0xff, 0x9e, 0x64),
+        VisualRole.CodePunctuation => new(0x2a, 0xc3, 0xde),
+        VisualRole.CodeFunctionName => new(0x7a, 0xa2, 0xf7),
+        VisualRole.CodeAttribute => new(0x2a, 0xc3, 0xde),
+        _ => new(0xc0, 0xca, 0xf5)
     };
 }
--- tests/TerminalDotnet.Tests/WhenStylingSourcePreview.cs
+++ tests/TerminalDotnet.Tests/WhenStylingSourcePreview.cs
@@ -7,16 +7,42 @@ namespace TerminalDotnet.Tests.Explorer;
 public sealed class WhenStylingSourcePreview
 {
     [Fact]
-    public void It_gives_keywords_and_types_distinct_colours()
+    public void It_uses_the_neovim_tokyonight_palette()
     {
         // Act
-        var colours = new[]
+        var colours = new Dictionary<VisualRole, Color>
         {
-            PreviewCodeAppearance.ForegroundFor(VisualRole.CodeKeyword),
-            PreviewCodeAppearance.ForegroundFor(VisualRole.CodeType)
+            [VisualRole.CodeComment] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeComment),
+            [VisualRole.CodeKeyword] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeKeyword),
+            [VisualRole.CodeString] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeString),
+            [VisualRole.CodeNumber] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeNumber),
+            [VisualRole.CodeOperator] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeOperator),
+            [VisualRole.CodeType] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeType),
+            [VisualRole.CodePreprocessor] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodePreprocessor),
+            [VisualRole.CodeIdentifier] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeIdentifier),
+            [VisualRole.CodeConstant] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeConstant),
+            [VisualRole.CodePunctuation] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodePunctuation),
+            [VisualRole.CodeFunctionName] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeFunctionName),
+            [VisualRole.CodeAttribute] = PreviewCodeAppearance.ForegroundFor(VisualRole.CodeAttribute)
         };
 
         // Assert
-        Assert.Equal(2, colours.Distinct().Count());
+        Assert.Equal(
+            new Dictionary<VisualRole, Color>
+            {
+                [VisualRole.CodeComment] = new(0x56, 0x5f, 0x89),
+                [VisualRole.CodeKeyword] = new(0x7d, 0xcf, 0xff),
+                [VisualRole.CodeString] = new(0x9e, 0xce, 0x6a),
+                [VisualRole.CodeNumber] = new(0xff, 0x9e, 0x64),
+                [VisualRole.CodeOperator] = new(0x89, 0xdd, 0xff),
+                [VisualRole.CodeType] = new(0x2a, 0xc3, 0xde),
+                [VisualRole.CodePreprocessor] = new(0x7d, 0xcf, 0xff),
+                [VisualRole.CodeIdentifier] = new(0xbb, 0x9a, 0xf7),
+                [VisualRole.CodeConstant] = new(0xff, 0x9e, 0x64),
+                [VisualRole.CodePunctuation] = new(0x2a, 0xc3, 0xde),
+                [VisualRole.CodeFunctionName] = new(0x7a, 0xa2, 0xf7),
+                [VisualRole.CodeAttribute] = new(0x2a, 0xc3, 0xde)
+            },
+            colours);
     }
 }
